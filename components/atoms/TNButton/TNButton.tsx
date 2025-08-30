1929012cab9c73a506c08ca76b5b03f63d5f04b4
Daniel Githiomi <danielgithiomi@gmail.com>
2025-08-30 08:53:36 +0400

    Added TNButton component with variants, types, and integration in HomeScreen.
    Signed-off-by: Daniel Githiomi <danielgithiomi@gmail.com>

---
+++ components/atoms/TNButton/TNButton.tsx
@@ -0,0 +1,105 @@
+import {FC} from "react";
+import {ButtonPadding, ButtonRounded, ButtonVariant, ContentPosition,} from "@/components";
+import {TNButtonProps} from "@tn/atoms/TNButton";
+import {Text, TouchableOpacity} from "react-native";
+import {cn} from "@/packages/libs";
+import {useAppTheme} from "@tn/hooks/useAppTheme";
+import {Colors} from "@/constants";
+
+export const TNButton: FC<TNButtonProps> = ({
+                                                id,
+                                                icon,
+                                                label,
+                                                onPress,
+                                                disabled,
+                                                buttonRef,
+                                                className,
+                                                labelStyle,
+                                                rounded = "small",
+                                                padding = "medium",
+                                                clickOpacity = 0.75,
+                                                variant = "default",
+                                                contentPosition = "center",
+                                                ...props
+                                            }) => {
+    const theme = useAppTheme();
+
+    console.log("Theme", theme);
+
+    const buttonColor = Colors[theme].text;
+    const buttonTextColor = Colors[theme].background;
+
+    const VARIANT_MAP: Record<ButtonVariant, string> = {
+        ghost: `bg-transparent`,
+        default: `bg-[${buttonColor}]`,
+        outline: `bg-transparent border border-[1.5px] border-[${buttonColor}]`,
+    };
+
+    const ROUNDED_MAP: Record<ButtonRounded, string> = {
+        none: "rounded-none",
+        small: "rounded-sm",
+        medium: "rounded-md",
+        large: "rounded-lg",
+        full: "rounded-full",
+    };
+
+    const PADDING_MAP: Record<ButtonPadding, string> = {
+        none: "p-0",
+        small: "p-2",
+        medium: "p-3",
+        large: "p-4",
+    };
+
+    const POSITION_MAP: Record<ContentPosition, string> = {
+        center: "justify-center",
+        left: "justify-start",
+        right: "justify-end",
+        between: "justify-between",
+        around: "justify-around",
+        evenly: "justify-evenly",
+    };
+
+    const buttonClasses = cn(
+        "flex flex-row items-center",
+        {
+            "flex-col": icon?.position === "top",
+            "flex-col-reverse": icon?.position === "bottom",
+            "flex-row-reverse": icon?.position === "right",
+        },
+        VARIANT_MAP[variant],
+        ROUNDED_MAP[rounded],
+        PADDING_MAP[padding],
+        POSITION_MAP[contentPosition],
+        className
+    );
+
+    console.log("Button classes", buttonClasses);
+
+    function handleOnPress() {
+        if (disabled) return;
+        onPress?.();
+    }
+
+    return (
+        <TouchableOpacity
+            id={id}
+            ref={buttonRef}
+            disabled={disabled}
+            onPress={handleOnPress}
+            className={buttonClasses}
+            activeOpacity={clickOpacity}
+            {...props}
+        >
+            {icon && icon.icon}
+            <Text
+                // style={{
+                //     color:
+                //         variant === "default" ? buttonTextColor : buttonColor,
+                // }}
+                className={labelStyle}
+            >
+                {label}
+            </Text>
+        </TouchableOpacity>
+    );
+};
--- components/atoms/index.ts
+++ components/atoms/index.ts
@@ -1,1 +1,2 @@
-export * from './TNText';
+export * from "./TNText";
+export * from "./TNButton";
