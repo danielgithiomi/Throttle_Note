7b47b69f0166f44dd089fbb670d5fc7761df923f
Daniel Githiomi <danielgithiomi@gmail.com>
2025-09-07 14:03:09 +0400

    Added `StyledText` stories, new Storybook addons, and updated preview decorators
    Signed-off-by: Daniel Githiomi <danielgithiomi@gmail.com>

--- .rnstorybook/preview.tsx
+++ .rnstorybook/preview.tsx
@@ -1,4 +1,8 @@
-import type { Preview } from '@storybook/react-native';
+import {View} from "react-native";
+import {Colors} from "@/constants/Colors";
+import {useColorScheme} from "@/hooks/useColorScheme";
+import type {Preview} from '@storybook/react-native';
+
 
 const preview: Preview = {
   parameters: {
@@ -9,6 +13,23 @@ const preview: Preview = {
       },
     },
   },
+    decorators: [
+        (Story, {parameters}) => {
+            const theme = useColorScheme() ?? "light";
+
+            return (
+                <View style={{
+                    flex: 1,
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    padding: 16,
+                    backgroundColor: parameters.backgroundColor || Colors[theme].tint
+                }}>
+                    <Story/>
+                </View>
+            )
+        }
+    ]
 };
 
 export default preview;
