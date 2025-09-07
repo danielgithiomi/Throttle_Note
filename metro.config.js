9354e39777ea207216cc6c1accdb878f0395014f
Daniel Githiomi <danielgithiomi@gmail.com>
2025-09-07 09:38:06 +0400

    storybook runs with only warnings. not perfect

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,9 @@
+const { withNativeWind } = require("nativewind/metro");
 const { getDefaultConfig } = require("expo/metro-config");
-const { withNativeWind } = require('nativewind/metro');
+const withStorybook = require("@storybook/react-native/metro/withStorybook");
 
-const config = getDefaultConfig(__dirname)
+const config = getDefaultConfig(__dirname);
 
-module.exports = withNativeWind(config, { input: './global.css' })
+module.exports = withStorybook(
+  withNativeWind(config, { input: "./global.css" })
+);
